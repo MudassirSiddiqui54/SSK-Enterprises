2ed7d8fd6ed8c79f31115d38bca52bd55adba67f
MudassirSiddiqui54 <mudassirsid54@gmail.com>
2026-03-04 22:11:21 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/SSK-Enterprises/",
   plugins: [react()],
   resolve: {
     alias: {
